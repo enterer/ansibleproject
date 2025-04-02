1eaa35284e36eb239e27696009e2df3f038aec86
enterer <fiyinsodipo@gmail.com>
2025-04-02 17:36:44 -0400

    update

--- ansible/roles/mongodb/files/init_db.js
+++ ansible/roles/mongodb/files/init_db.js
@@ -2,5 +2,5 @@ use securedb;
 db.messages.insertMany([
   { text: "Hello from MongoDB!" },
   { text: "This app uses HTTPS via Let's Encrypt" },
-  { text: "Deployed securely with Ansible and Jenkins" }
+  { text: "Deployed securely with Ansible and Jenkins by Yaro, Wole and MJ" }
 ]);

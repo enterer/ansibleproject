e4433496ff98b1e63e2361ca3ee53b87ce40f286
enterer <fiyinsodipo@gmail.com>
2025-04-02 17:18:05 -0400

    updates

--- ansible/roles/flask_app/files/templates/init_db.js
+++
@@ -1,6 +0,0 @@
-use securedb;
-db.messages.insertMany([
-  { text: "Hello from MongoDB!" },
-  { text: "This app uses HTTPS via Let's Encrypt" },
-  { text: "Deployed securely with Ansible and Jenkins" }
-]);

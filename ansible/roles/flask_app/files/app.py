c8dd8be7f5e115fb66698e16168934b44d61d858
enterer <fiyinsodipo@gmail.com>
2025-04-06 01:24:58 -0400

    Update app.py

--- ansible/roles/flask_app/files/app.py
+++ ansible/roles/flask_app/files/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from pymongo import MongoClient
+from datetime import datetime
 
 app = Flask(__name__)
 client = MongoClient("mongodb://localhost:27017/")
@@ -9,7 +10,8 @@ collection = db.messages
 @app.route("/")
 def index():
     messages = collection.find()
-    return render_template("index.html", messages=messages)
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return render_template("index.html", messages=messages, deployed_at=timestamp)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
